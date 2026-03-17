f011cb98db8faf5b252890b875ca126272c6e732
luketanner298 <luketanner2908@icloud.com>
2026-03-17 10:51:04 +0000

    nothing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,6 +1,4 @@
-using Unity.VisualScripting;
 using UnityEngine;
-using UnityEngine.EventSystems;
 
 public class PlayerController : MonoBehaviour
 {
@@ -8,28 +6,19 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private GameObject player;
     [SerializeField] private Camera cam;
     [SerializeField] private float Sensitivity;
+
     [SerializeField] private float speed, walk, run, crouch;
 
     private Vector3 crouchScale, normalScale;
 
-    public bool isMoving, isCrouching, isRunning, isGrounded;
+    public bool isMoving, isCrouching, isRunning;
+
     private float X, Y;
-    public float jumpHeight = 15.0f;
-    public float fallSpeed = -10.0f;
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundmask;
-    public AudioClip jumpSound;
-    private Vector3 velocity;
 
-
-    private float timer;
-    private Vector3 defaultPosition;
-
-    public void Start()
+    private void Start()
     {
         speed = walk;
-        crouchScale = new Vector3(1, .50f, 1);
+        crouchScale = new Vector3(1, .75f, 1);
         normalScale = new Vector3(1, 1, 1);
         cc = GetComponent<CharacterController>();
         cc.enabled = true;
@@ -53,20 +42,12 @@ public class PlayerController : MonoBehaviour
         #endregion
         transform.localRotation = Quaternion.Euler(Y, X, 0.0f);
 
-        // Calculates if player is on the ground using the groundCheck gameobject
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundmask);
-
-        if (isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
         Vector3 forward = transform.forward * vertical;
         Vector3 right = transform.right * horizontal;
 
-            cc.SimpleMove(Vector3.Normalize(forward + right) * speed);
+        cc.SimpleMove(Vector3.Normalize(forward + right) * speed);
         // Determines if the speed = run or walk
         if (Input.GetKey(KeyCode.LeftShift))
         {
@@ -89,18 +70,7 @@ public class PlayerController : MonoBehaviour
             player.transform.localScale = normalScale;
         }
         // Detects if the player is moving.
-        // Useful for footstep sounds.
+        // Useful if you want footstep sounds and or other features in your game.
         isMoving = cc.velocity.sqrMagnitude > 0.0f;
-
-        // Jumping mechanic
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * fallSpeed);
-        }
-
-        // If player is in the air they fall
-        velocity.y += fallSpeed * Time.deltaTime;
-
     }
-
 }
